6ea65c5e099980d72be68866e8431eaf2d558a47
Pedrohrd <pedrohrd87@gmail.com>
2025-01-23 10:56:11 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import * as dotenv from 'dotenv'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -18,6 +19,6 @@ async function bootstrap() {
       },
     }),
   )
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 4000)
 }
 bootstrap()
