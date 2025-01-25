4f82871571ef7bfbd2f67c741072ef93741e5a9e
Pedrohrd <pedrohrd87@gmail.com>
2025-01-25 10:18:37 -0300

    .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,3 @@
-import { MailerModule, MailerService } from '@nestjs-modules/mailer'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
@@ -12,27 +11,6 @@ import { GoogleStrategy } from './strategies/google.strategy'
 @Module({
   controllers: [AuthController],
   providers: [AuthService, JwtService, GoogleStrategy],
-  imports: [
-    PassportModule,
-    UserModule,
-    ConfigModule.forRoot(),
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          auth: {
-            user: config.get<string>('EMAIL_USER'),
-            pass: config.get<string>('EMAIL_PASS'),
-          },
-        },
-        defaults: {
-          from: '"No Reply" <no-reply@seu-dominio.com>',
-        },
-      }),
-    }),
-  ],
+  imports: [PassportModule, UserModule, ConfigModule.forRoot()],
 })
 export class AuthModule {}
